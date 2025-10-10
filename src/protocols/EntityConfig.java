2c553ee068f0815462685ada9fe4d3ff701b3fb3
iLorenzz <ldutra0912@gmail.com>
2025-10-09 21:10:06 -0300

    fix: implement configuration entity as a separate class

---
+++ src/protocols/EntityConfig.java
@@ -0,0 +1,21 @@
+public class EntityConfig {
+    private final String host_name;
+    private final int port_number;
+
+    public EntityConfig(String host_name, int port_number)throws Exception{
+        this.host_name = host_name;
+        if(port_number <= 1024 || port_number > 65535){
+            throw new Exception();
+        }
+
+        this.port_number = port_number;
+    }
+
+    public int getPort_number() {
+        return port_number;
+    }
+
+    public String getHost_name() {
+        return host_name;
+    }
+}
--- src/protocols/UnicastProtocol.java
+++ src/protocols/UnicastProtocol.java
@@ -5,26 +5,18 @@ import java.util.List;
 import java.util.concurrent.ConcurrentMap;
 
 public class UnicastProtocol implements UnicastServiceInterface, Runnable{
-    private static ConcurrentMap<Short, List<String>> entity_map;
+    private static ConcurrentMap<Short, List<EntityConfig>> entity_map;
 
     private short ucsap_id;
     private InetAddress host_name;
-    private int port_number;
 
-    public UnicastProtocol(short ucsap_id, String host_name, int port_number) throws Exception{
+    public UnicastProtocol(short ucsap_id, String host_name) throws Exception{
         this.ucsap_id = ucsap_id;
         try {
             this.host_name = InetAddress.getByName(host_name);
         }catch(UnknownHostException e) {
             System.out.println(e.getMessage());
         }
-
-        if(port_number <= 1024 || port_number > 65535){
-            throw new Exception();
-        }
-
-        this.port_number = port_number;
-
     }
 
     @Override
