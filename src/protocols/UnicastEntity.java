a732b59d9314db864f4b4597992ad90c957fed6e
iLorenzz <ldutra0912@gmail.com>
2025-10-17 16:34:34 -0300

    refactor: implement new UnicastProtocol class signture

--- src/protocols/UnicastEntity.java
+++ src/protocols/UnicastEntity.java
@@ -1,4 +1,11 @@
-public class UnicastEntity {
+import java.io.File;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+public class UnicastEntity implements  Runnable{
+
+    private short ucsap_id;
     private final String host_name;
     private final int port_number;
 
@@ -7,6 +14,12 @@ public class UnicastEntity {
         if(port_number <= 1024 || port_number > 65535){
             throw new Exception();
         }
+
+        /*try {
+            this.host_name = InetAddress.getByName(host_name);
+        }catch(UnknownHostException e) {
+            System.out.println(e.getMessage());
+        }*/
 
         this.port_number = port_number;
     }
@@ -17,5 +30,38 @@ public class UnicastEntity {
 
     public String getHost_name() {
         return host_name;
+    }
+
+    @Override
+    public void run() {
+
+    }
+
+    //put in main file
+    private UnicastEntity read_config_file() throws Exception{
+        String configuration_file_path = "configuration.txt";
+        String[] found_correct_configuration = null;
+
+        try(Scanner sc = new Scanner(new File(configuration_file_path))){
+            while(sc.hasNextLine()){
+                String config = sc.nextLine();
+
+                String[] separated_configs = config.split("");
+                if(Short.parseShort(separated_configs[0]) == ucsap_id){
+                    found_correct_configuration = separated_configs;
+                    break;
+                }
+            }
+
+            if(found_correct_configuration != null){
+                return new UnicastEntity(found_correct_configuration[1], Integer.parseInt(found_correct_configuration[2]));
+            }
+
+        }catch(Exception e){
+            System.err.println(e.getMessage());
+        }
+
+        //TODO  implement exception if this ucsap_id was not found
+        throw new Exception();
     }
 }
--- src/protocols/UnicastProtocol.java
+++ src/protocols/UnicastProtocol.java
@@ -1,61 +1,19 @@
-import java.io.File;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
-import java.util.Scanner;
 import java.util.concurrent.ConcurrentMap;
 
-public class UnicastProtocol implements UnicastServiceInterface, Runnable{
+public class UnicastProtocol implements UnicastServiceInterface{
     private static ConcurrentMap<Short, UnicastEntity> entity_map;
 
-    private short ucsap_id;
     private InetAddress host_name;
 
-    public UnicastProtocol(short ucsap_id, String host_name) throws Exception{
-        this.ucsap_id = ucsap_id;
-        try {
-            this.host_name = InetAddress.getByName(host_name);
-        }catch(UnknownHostException e) {
-            System.out.println(e.getMessage());
-        }
+    public UnicastProtocol(InetAddress host_name) throws Exception{
 
-        UnicastEntity this_unicast_entity_config = read_config_file();
-        entity_map.put(ucsap_id, this_unicast_entity_config);
-    }
-
-    private UnicastEntity read_config_file() throws Exception{
-        String configuration_file_path = "configuration.txt";
-        String[] found_correct_configuration = null;
-
-        try(Scanner sc = new Scanner(new File(configuration_file_path))){
-            while(sc.hasNextLine()){
-                String config = sc.nextLine();
-
-                String[] separated_configs = config.split("");
-                if(Short.parseShort(separated_configs[0]) == ucsap_id){
-                    found_correct_configuration = separated_configs;
-                    break;
-                }
-            }
-
-            if(found_correct_configuration != null){
-                return new UnicastEntity(found_correct_configuration[1], Integer.parseInt(found_correct_configuration[2]));
-            }
-
-        }catch(Exception e){
-            System.err.println(e.getMessage());
-        }
-
-        //TODO  implement exception if this ucsap_id was not found
-        throw new Exception();
+        this.host_name = host_name;
     }
 
     @Override
     public boolean up_data_req(short destination, String message){
         return true;
-    }
-
-    @Override
-    public void run() {
-
     }
 }
